912b5745de3f9dc4da74512fc3146bf88ed5c541
annie <anyfuncy@hotmail.com>
2023-05-30 08:03:13 +0100

    use verbose for rbenv ruby install and change ruby version to 2.7.3

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -36,7 +36,7 @@ eval "$(rbenv init -)"
 ruby_version=$(cat .ruby-version)
 if [ -z "$(rbenv version | grep $ruby_version)" ]; then
   echo "--- ⚠️  Installing Ruby $ruby_version..."
-  rbenv install --skip-existing $ruby_version
+  rbenv install --skip-existing --verbose  $ruby_version
   rbenv shell $ruby_version
 else
   echo "--- ✅  Ruby $ruby_version already installed"
